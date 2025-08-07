1c628f16bd293a65923c0cf3997f70598cc79342
mostafa-abokhadra <mvxizk@gmail.com>
2025-08-07 18:24:59 +0300

    creating sessionConfig middleware, adding @type/express-session for dev-dependencies, modifying ts.config file

---
+++ src/config/session.ts
@@ -0,0 +1,19 @@
+import session from "express-session";
+import dotenv from "dotenv"
+
+dotenv.config()
+
+const sessionConfig: session.SessionOptions = {
+    secret: process.env.SESSION_SECRET!,
+    cookie: {
+        maxAge: 24 * 60 * 60 * 1000,
+        httpOnly: true,
+        // secure: process.env.NODE_ENV === 'production',
+        // sameSite: 'strict'
+    },
+    resave: false,
+    saveUninitialized: false,
+    // store: 
+}
+
+export default session(sessionConfig);
