ac36961722a4284a30b21e81042f2e80dc8dbce4
mostafa-abokhadra <mvxizk@gmail.com>
2025-08-15 21:13:21 +0300

    creating password and confirm password fields validation logic

--- src/validation/auth/signupValidator.ts
+++ src/validation/auth/signupValidator.ts
@@ -2,7 +2,43 @@ import {body} from "express-validator"
 
 const emailValidator = [
     body('email')
+    .trim()
     .not().isEmpty().withMessage('Email Is Required')
     .isEmail().withMessage('Invalide Email Address')
     .normalizeEmail()
+]
+
+const passwordValidator = [
+    body('password')
+    .not().isEmpty().withMessage('Password is Required')
+    .isLength({min: 12}).withMessage('Must Contain at least 12 Character')
+    .custom((password) => {
+        for(let i = 0; i < password.length; i++) {
+            if (password.charCodeAt(i) >= 48 && password.charCodeAt(i) <= 57)
+                return true
+        }
+        throw new Error('Must Contain at Least One Number')
+    })
+    .custom((password) => {
+        for(let i = 0; i < password.length; i++) {
+            if (password.charCodeAt(i) >= 65 && password.charCodeAt(i) <= 90)
+                return true
+        }
+        throw new Error('Must Contain at Least 1 Capital Letter')
+    })
+    .custom((password) => {
+        for(let i = 0; i < password.length; i++) {
+            if (password.charCodeAt(i) >= 97 && password.charCodeAt(i) <= 122)
+                return true
+        }
+        throw new Error('Must Contain at Least 1 Small Letter')
+    }),
+
+    body('confirmPassword')
+    .notEmpty().withMessage('Confirm password Field is Required')
+    .custom((password, { req }) => {
+        if (password == req.body.password) 
+            return true;
+        throw new Error('Passwords do not Match');
+    }),
 ]
