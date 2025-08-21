6019a4caccc4990f5b992b3b2bbd4bbc260746b9
mostafa-abokhadra <mvxizk@gmail.com>
2025-08-21 09:09:31 +0300

    adding custom css file to swagger specification

--- src/utils/swagger.ts
+++ src/utils/swagger.ts
@@ -10,10 +10,15 @@ const __dirname = path.dirname(__filename);
 const router = express.Router()
 const swaggerDoc = YAML.load(path.resolve(__dirname, '../../src/docs/swagger.yaml'))
 
+const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css"
+
 router.use(
     '/api-docs',
     swaggerUi.serve,
-    swaggerUi.setup(swaggerDoc)
+    swaggerUi.setup(swaggerDoc, {
+        customCss: '.swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }',
+        customCssUrl: CSS_URL,
+    })
 )
 
 export default router;
