93f211a6f99afad2985f22c908c24e42fd5b1b45
mostafa-abokhadra <mvxizk@gmail.com>
2025-08-24 15:18:34 +0300

    adding checkRedisConnection middleware to singup route

--- src/routes/auth/signupRoutes.ts
+++ src/routes/auth/signupRoutes.ts
@@ -1,6 +1,7 @@
 import  express  from "express";
 
 import { blockAuthenticatedUser } from "../../middlewares/sessionManage.js";
+import checkRedisConnection from "../../middlewares/checkRedisConnection.js";
 import { emailValidator, passwordValidator } from "../../validation/auth/signupValidator.js";
 import handleValidationError from "../../middlewares/handleValidationErrors.js";
 import signupController from "../../controllers/auth/signupController.js";
@@ -10,6 +11,7 @@ const router = express.Router()
 router.post(
     '/signup',
     blockAuthenticatedUser,
+    checkRedisConnection,
     emailValidator,
     passwordValidator,
     handleValidationError,
