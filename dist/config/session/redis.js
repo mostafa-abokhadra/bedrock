c901337ef20fdb9a7ab1e720fbaf0ca0131a0209
mostafa-abokhadra <mvxizk@gmail.com>
2025-08-24 15:04:54 +0300

    adding checkRedisConnection middleware to prevent authentication if redis is not available

--- dist/config/session/redis.js
+++ dist/config/session/redis.js
@@ -1,14 +1,24 @@
 import { createClient } from 'redis';
 import { RedisStore } from 'connect-redis';
+export let redisConnected = false;
 const redisClient = createClient({
-    url: process.env.REDIS_SESSION_CACHE_URL
+    url: process.env.REDIS_SESSION_CACHE_URL,
+    socket: {
+        reconnectStrategy: false,
+    },
 });
-redisClient.on("error", (err) => {
-    console.log("Redis client error", err);
+redisClient.on("error", () => {
+    redisConnected = false;
 });
 redisClient.on("connect", () => {
-    console.log("connected to redis");
+    redisConnected = true;
 });
-await redisClient.connect();
+try {
+    await redisClient.connect();
+    redisConnected = true;
+}
+catch (err) {
+    redisConnected = false;
+}
 const sessionStore = new RedisStore({ client: redisClient });
 export default sessionStore;
