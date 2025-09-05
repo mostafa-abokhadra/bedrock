7c58ca6ecc7fd88465c6cf97eee50b0a879106eb
mostafa-abokhadra <mbukhadra@gmail.com>
2025-09-06 01:10:37 +0300

    adding cors logic

--- dist/config/session/session.js
+++ dist/config/session/session.js
@@ -9,6 +9,7 @@ const sessionConfig = {
         maxAge: 24 * 60 * 60 * 1000,
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production',
+        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax'
     },
     resave: false,
     saveUninitialized: false,
--- src/server.ts
+++ src/server.ts
@@ -9,16 +9,46 @@ import apiDocsRoute from './utils/swagger.js'
 import loginRoutes from './routes/auth/loginRoutes.js'
 import signupRoutes from "./routes/auth/signupRoutes.js";
 import csrfRoutes from "./routes/auth/csrfRoute.js";
+import cors from "cors";
 
 const server = express()
 server.set('trust proxy', 1);
 server.use(express.json())
 server.use(express.urlencoded({extended: true}))
 
+server.use(cors({
+    origin:  true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+}))
+
 server.use(session)
 server.use(passport.initialize())
 server.use(passport.session())
 
+server.use((req, res, next) => {
+    console.log('Session ID:', req.sessionID);
+    console.log('Session:', req.session);
+    next();
+});
+server.get('/debug-session', (req, res) => {
+    res.json({
+        sessionId: req.sessionID,
+        sessionExists: !!req.session,
+        cookies: req.headers.cookie,
+        headers: req.headers,
+        secure: req.secure,
+        hostname: req.hostname
+    });
+});
+
+// Add this to check if cookies are being set
+server.use((req, res, next) => {
+    res.on('finish', () => {
+        console.log('Response headers:', res.getHeaders());
+    });
+    next();
+});
 server.use('/auth', loginRoutes);
 server.use('/auth', signupRoutes);
 server.use('/auth', csrfRoutes);
