6fa2bb09259fdd8882b704384f893abfd7dbae63
mostafa-abokhadra <mbukhadra@gmail.com>
2025-09-05 23:32:05 +0300

    fixing: removing dist folder from gitignore

---
+++ dist/middlewares/csrfProtection.js
@@ -0,0 +1,11 @@
+import csurf from "csurf";
+const csrfProtection = csurf({ cookie: true });
+const csrfMiddleware = (req, res, next) => {
+    csrfProtection(req, res, (err) => {
+        if (err && err.code === 'EBADCSRFTOKEN') {
+            return res.status(403).json({ message: 'Invalid CSRF Token' });
+        }
+        return next();
+    });
+};
+export default csrfMiddleware;
