d5254e95c8cbf41f297358a91cb4c580cb274bd5
mostafa-abokhadra <mbukhadra@gmail.com>
2025-09-14 00:55:46 +0300

    create create new vault controller

--- src/routes/vaultRoutes.ts
+++ src/routes/vaultRoutes.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import { isAuthenticated } from '../middlewares/sessionManage.js';
 import csrfMiddleware from '../middlewares/csrfProtection.js';
-
+import vaultController from '../controllers/vaultController.js'; 
 const router = express.Router();
 
 router.post(
