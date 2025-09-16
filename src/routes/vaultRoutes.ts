63a07fa4a0a844c4f2d61b61e9ac22a78c01c964
mostafa-abokhadra <mbukhadra@gmail.com>
2025-09-16 20:23:32 +0300

    adding name validator for the vault validation

--- src/routes/vaultRoutes.ts
+++ src/routes/vaultRoutes.ts
@@ -2,12 +2,17 @@ import express from 'express';
 import { isAuthenticated } from '../middlewares/sessionManage.js';
 import csrfMiddleware from '../middlewares/csrfProtection.js';
 import vaultController from '../controllers/vaultController.js'; 
+import { nameValidator } from '../validation/vaultValidator.js';
+import handleValidationError from '../middlewares/handleValidationErrors.js';
+
 const router = express.Router();
 
 router.post(
     '/vaults',
     isAuthenticated,
     csrfMiddleware,
+    nameValidator,
+    handleValidationError,
     vaultController.createVault
 )
 export default router;;
