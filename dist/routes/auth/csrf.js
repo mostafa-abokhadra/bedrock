c4766d849ebc83f9b78853b1f5c0de20ef12eb1d
mostafa-abokhadra <mbukhadra@gmail.com>
2025-10-31 22:58:08 +0200

    removing dist folder from public repo

--- dist/routes/auth/csrf.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import csrf from "csurf";
-const router = Router();
-const csrfProtection = csrf({ cookie: true });
-router.get('/csrf-token', csrfProtection, (req, res) => {
-    return res.json({ csrfToken: req.csrfToken() });
-});
-export default router;
