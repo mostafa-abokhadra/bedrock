9b021cbd0611f92ec2b2b6ac4f434d51d99ca223
mostafa-abokhadra <mbukhadra@gmail.com>
2025-11-06 07:26:11 +0200

    Migrating to Mongodb: create vault schema, updating user schema, handling one to many relation between user and vault models, refactor create new vault logic to use mongoose

--- src/controllers/vaultController.ts
+++ src/controllers/vaultController.ts
@@ -1,24 +1,23 @@
-import { PrismaClient } from "@prisma/client";
-const prisma = new PrismaClient();
-
+import { Vault } from "../models/vault.model.js";
+import { User } from "../models/user.model.js";
 export default class vaultController {
     static async createVault(req: any, res: any) {
         const { name } = req.body;
-
         try { 
-            const newVault = await prisma.vault.create({
-                data: {
-                    name,
-                    createdAt: new Date(),
-                    updatedAt: new Date(),
-                    userId: req.user.id,
-                }
-            });
+            console.log(req.user)
+            const newVault = await Vault.create({name, author: req.user._id})
             if (!newVault) {
                 return res.status(400).json({ message: "vault creation failed" });
             }
+            console.log("the new vault", newVault)
+            const updateUserVaults = await User.findByIdAndUpdate(
+                req.user._id,
+                { $addToSet: { vaults: newVault._id}},
+                {new: true}
+            )
             return res.status(201).json({ message: "Vault created successfully", vault: newVault });
         } catch (error) {
+            console.log(error)
             return res.status(500).json({ message: "Internal server error", error: error });  
         }
     }
