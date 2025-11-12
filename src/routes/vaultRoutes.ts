1a24b4c70123c6ac0b9c44cea8ee94bcd28e5dcf
mostafa-abokhadra <mbukhadra@gmail.com>
2025-11-12 14:40:05 +0200

    added a new vault route: adding route to update vault name, adding updateVaultName controller, adding old name and new name validators, drop vault name unique indx and replacing it with authom_vaultName unique index, refactoring 500 status error message response

--- src/routes/vaultRoutes.ts
+++ src/routes/vaultRoutes.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import { isAuthenticated } from '../middlewares/sessionManage.js';
 import csrfMiddleware from '../middlewares/csrfProtection.js';
 import vaultController from '../controllers/vaultController.js'; 
-import {nameValidator} from '../validation/vaultValidator.js';
+import {nameValidator, oldNameValidator, newNameValidator} from '../validation/vaultValidator.js';
 import handleValidationError from '../middlewares/handleValidationErrors.js';
 
 const router = express.Router();
@@ -22,4 +22,13 @@ router.get(
     vaultController.getVaults
 )
 
+router.put(
+    "/vaults",
+    isAuthenticated,
+    csrfMiddleware,
+    oldNameValidator,
+    newNameValidator,
+    handleValidationError,
+    vaultController.updateVault
+)
 export default router;;
