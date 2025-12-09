f99d966ac1cc7559a972845424bf14a9c0def528
mostafa-abokhadra <mbukhadra@gmail.com>
2025-12-09 23:51:17 +0200

    adding folder name validator

--- src/validation/folderValidator.ts
+++ src/validation/folderValidator.ts
@@ -1,6 +1,13 @@
-import { param } from "express-validator";
+import { body, param } from "express-validator";
 
 const vaultIdValidator = [
     param("vaultId")
     .not().isEmpty().withMessage("vault id is required")
+]
+
+const nameValidator = [
+    body("name")
+    .not().isEmpty().withMessage("folder name is required")
+    .isString().withMessage("folder name must be a string")
+    .isLength({max: 30}).withMessage("folder name must not excced 30 character")
 ]
