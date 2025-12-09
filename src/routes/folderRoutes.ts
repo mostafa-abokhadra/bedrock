26d0fff890237c7a7fc36002158806460ec7fe98
mostafa-abokhadra <mbukhadra@gmail.com>
2025-12-10 00:02:41 +0200

    adding name and parentId validators middleware to the create folder route

--- src/routes/folderRoutes.ts
+++ src/routes/folderRoutes.ts
@@ -1,12 +1,18 @@
 import { Router } from "express";
 import { isAuthenticated } from "../middlewares/sessionManage.js";
 import csrfMiddleware from "../middlewares/csrfProtection.js";
+import { parentIdValidator, nameValidator} from "../validation/folderValidator.js";
+import handleValidationError from "../middlewares/handleValidationErrors.js";
 const router = Router()
 
 router.post(
     '/folders',
     isAuthenticated,
     csrfMiddleware,
+    parentIdValidator,
+    nameValidator,
+    handleValidationError,
+    
 )
 
 export default router;
